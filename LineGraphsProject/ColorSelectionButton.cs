0194ecc70cc806853966a58c37b0694d62db53cc
yuo123 <aaa444777@gmail.com>
2017-02-26 08:22:00 +0200

    Imported ColorSelectionButton to be used in AddGraphForm

---
+++ LineGraphsProject/ColorSelectionButton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace LineGraphsProject
+{
+    public class ColorSelectionButton : Button
+    {
+        private static ColorDialog m_diag = new ColorDialog();
+
+        public override string Text
+        {
+            get
+            {
+                return "";
+            }
+
+            set
+            {
+                return;
+            }
+        }
+
+        public ColorSelectionButton()
+        {
+            this.FlatStyle = FlatStyle.Flat;
+            this.Text = "";
+            this.UseVisualStyleBackColor = false;
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            if (m_diag.ShowDialog() == DialogResult.OK)
+                this.BackColor = m_diag.Color;
+            base.OnClick(e);
+        }
+    }
+}

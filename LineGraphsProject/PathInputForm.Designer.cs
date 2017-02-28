1c798ca73e371911ba8670c1fb7f3ac902cafb67
yuo123 <aaa444777@gmail.com>
2017-02-28 18:04:14 +0200

    Finished PathInputForm

--- LineGraphsProject/PathInputForm.Designer.cs
+++ LineGraphsProject/PathInputForm.Designer.cs
@@ -28,32 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.Label label1;
             this.pathBox = new System.Windows.Forms.TextBox();
             this.okBtn = new System.Windows.Forms.Button();
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // pathBox
             // 
-            this.pathBox.Location = new System.Drawing.Point(33, 91);
+            this.pathBox.Location = new System.Drawing.Point(69, 6);
             this.pathBox.Name = "pathBox";
-            this.pathBox.Size = new System.Drawing.Size(114, 20);
+            this.pathBox.Size = new System.Drawing.Size(181, 20);
             this.pathBox.TabIndex = 0;
+            this.pathBox.Validating += new System.ComponentModel.CancelEventHandler(this.pathBox_Validating);
             // 
             // okBtn
             // 
-            this.okBtn.Location = new System.Drawing.Point(131, 171);
+            this.okBtn.Location = new System.Drawing.Point(256, 32);
             this.okBtn.Name = "okBtn";
-            this.okBtn.Size = new System.Drawing.Size(80, 54);
+            this.okBtn.Size = new System.Drawing.Size(61, 35);
             this.okBtn.TabIndex = 1;
             this.okBtn.Text = "Ok";
             this.okBtn.UseVisualStyleBackColor = true;
             this.okBtn.Click += new System.EventHandler(this.okBtn_Click);
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(51, 13);
+            label1.TabIndex = 2;
+            label1.Text = "File Path:";
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.Location = new System.Drawing.Point(256, 6);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(61, 20);
+            this.browseBtn.TabIndex = 3;
+            this.browseBtn.Text = "Browse...";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            this.browseBtn.Click += new System.EventHandler(this.browseBtn_Click);
+            // 
             // PathInputForm
             // 
+            this.AcceptButton = this.okBtn;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(325, 75);
+            this.Controls.Add(this.browseBtn);
+            this.Controls.Add(label1);
             this.Controls.Add(this.okBtn);
             this.Controls.Add(this.pathBox);
             this.Name = "PathInputForm";
@@ -67,5 +94,7 @@
 
         private System.Windows.Forms.TextBox pathBox;
         private System.Windows.Forms.Button okBtn;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
     }
 }
